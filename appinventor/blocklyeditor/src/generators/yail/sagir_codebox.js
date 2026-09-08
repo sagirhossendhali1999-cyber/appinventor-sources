85f7a6bda32518ea1f7234b0446da48f8f597cc4
sagirhossendhali1999-cyber <sagirhossendhali1999@gmail.com>
2026-09-08 14:15:23 +0600

    Final universal code

--- appinventor/blocklyeditor/src/generators/yail/sagir_codebox.js
+++ appinventor/blocklyeditor/src/generators/yail/sagir_codebox.js
@@ -1,4 +1,5 @@
 Blockly.Yail['add_source_directly'] = function() {
-      var code = Blockly.Yail.valueToCode(this, 'CODE', Blockly.Yail.ORDER_NONE);
-        return code + '\n';
+    var code = Blockly.Yail.valueToCode(this, 'CODE', Blockly.Yail.ORDER_NONE);
+      var raw = code.slice(1, -1);
+        return raw + '\n';
         };
